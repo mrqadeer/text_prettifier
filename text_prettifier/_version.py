b9854066bc5996c50efa8a0fb44f4e49b38df5b4
mrqadeer <mrqadeer1231122@gmail.com>
2024-05-02 22:23:11 +0500

    working version

--- text_prettifier/_version.py
+++ text_prettifier/_version.py
@@ -5,7 +5,7 @@ version_json = '''
  "date": "2024-05-02T00:00:00-0000",
  "dirty": false,
  "error": null,
- "full-revisionid": "45eafa102436b5935b0b4879804c7eb5d8a0bdc8",
+ "full-revisionid": "f4c077b3cb914e1dc713ae127f2df68533682098",
  "version": "1.1.3"
 }
 '''  # END VERSION_JSON
