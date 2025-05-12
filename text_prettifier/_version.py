43127ec1d96a569f4b8f93b15b1a62fec6bf8a8d
mrqadeer <mrqadeer1231122@gmail.com>
2025-05-13 02:05:27 +0500

    Bump version to 2.0.0

--- text_prettifier/_version.py
+++ text_prettifier/_version.py
@@ -1,12 +1,11 @@
 import json
 
 version_json = '''
-{
- "date": "2024-05-03T00:00:00-0000",
+{ "date": "2024-05-03T00:00:00-0000",
  "dirty": false,
  "error": null,
  "full-revisionid": "83553a370d543990a0d3c352ea373926d4132dcb",
- "version": "1.2.0"
+ "version": "2.0.0"
 }
 '''  # END VERSION_JSON
 
